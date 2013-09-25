d7cbce4a1c50b482d303c1fade3eaa0a4fd3b20c
pwillia7 <ptk.williams@gmail.com>
2013-09-25 15:46:22 -0500

    redesign and added functionality

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+This is a quick project to quickly find lower BestBuy competitor prices. 
+
+Just enter the identifier code (UPC, EAN, ISBN) and search to retrieve results with
+prices and links to BestBuy competitor's products.
+
+Still plenty to do!
+
 Check out the preview here:
 http://htmlpreview.github.io/?https://github.com/pwillia7/Bestbuy-Competitor-Comparer/blob/master/index.html
--- main.js
+++ main.js
@@ -1,47 +1,35 @@
 function search(){
     console.log("searching");
- searchTerm = document.getElementById("searchbox").value;
-$.ajax({
-  url: "https://us.api.invisiblehand.co.uk/v1/products?query="+encodeURIComponent(searchTerm)+"&app_id=b99c5cf8&app_key=35c6118f97a3e2eedeeca5251fa5049e&size=100",
-  success: function(data, textStatus, jqXHR) {
-    for(i = 0; i <= data.results.length; i++){
-        //if retailer name matches, continue
-        var retailerName = data.results[i].best_page.retailer_name.toLowerCase();
-        if(retailerName === "target.com" || retailerName === "amazon.com" || retailerName === "tigerdirect.com" || retailerName === "bhphotovideo.com" || retailerName === "officedepot.com" || retailerName === "officemax.com" || retailerName === "crutchfield.com" || retailerName === "walmart.com" || retailerName === "sears.com" || retailerName === "hp.com" || retailerName === "homedepot.com" || retailerName === "lowes.com" || retailerName === "newegg.com"){
-        //create result container
-        resultContainer = document.createElement("div");
-            resultContainer.setAttribute("id", "result"+i+1);
-            resultContainer.setAttribute("class", "resultcontainer");
-            $(resultContainer).appendTo( "#wrapper" );
-        titleDiv = document.createElement("div");
-            titleDiv.setAttribute("class", "title");
-            Title = data.results[i].title;
-            titleDiv.innerHTML = Title;
-            $(titleDiv).appendTo( "#result"+i+1 );
-        retailerDiv = document.createElement("div");
-            retailerDiv.setAttribute("class", "retailer");
-            Retailer = data.results[i].best_page.retailer_name;
-            retailerDiv.innerHTML = Retailer;
-            $(retailerDiv).appendTo( "#result"+i+1);
-        priceDiv = document.createElement("div");
-            priceDiv.setAttribute("class", "price");
-            Price = data.results[i].best_page.price;
-            priceDiv.innerHTML = "$"+Price;
-            $(priceDiv).appendTo( "#result"+i+1);
-        linkDiv = document.createElement("div");
-            linkDiv.setAttribute("class", "link");
-            linkDiv.setAttribute("id", "link"+i+1);
-            Link = data.results[i].best_page.deeplink;
-            $(linkDiv).appendTo( "#result"+i+1);
-        linkAnchorDiv = document.createElement("a");
-            linkAnchorDiv.setAttribute("href", Link);
-            linkAnchorDiv.setAttribute("class", "linkAnchor");
-            linkAnchorDiv.innerHTML = "Link";
-            $(linkAnchorDiv).appendTo( "#link"+i+1);
-    }
-   
-         }
+	var searchTerm = document.getElementById("searchbox").value;
+	
+	$.ajax({
+		
+		url: "https://us.api.invisiblehand.co.uk/v1/products?identifier="+searchTerm+"&include_pages=true&app_id=b99c5cf8&app_key=35c6118f97a3e2eedeeca5251fa5049e&size=100",
+		success: function(data, textStatus, jqXHR) {
+			
+			//eliminate unimportant retailers
+			for( var i in data.results[0].pages ){
+				var retailerName= data.results[0].pages[i].retailer_name.toLowerCase();
+				if(retailerName === "target.com" || retailerName === "amazon.com" || retailerName === "tigerdirect.com" || retailerName === "bhphotovideo.com" || retailerName === "officedepot.com" || retailerName === "officemax.com" || retailerName === "crutchfield.com" || retailerName === "walmart.com" || retailerName === "sears.com" || retailerName === "hp.com" || retailerName === "homedepot.com" || retailerName === "lowes.com" || retailerName === "newegg.com"){
+					console.log(retailerName);
+					//create current object
+					var currentResult = data.results[0].pages[i];
+					console.log(currentResult);
+					var result = {
+						retailer:	currentResult.retailer_name,
+						price:		currentResult.price,
+						image:      currentResult.image_url,
+						link:		currentResult.deeplink,
+						title:		currentResult.title.slice(0,50)
+					};
+					console.log(result.retailer);
 
-     }
-    });}
-
+				
+					var resultTemplate = "<div class=\"result\"><div class=\"title\">{{title}}</div><div class=\"imageContainer\" style=\"background-image: url(\'{{image}}\');\"></div><div class=\"price\">${{price}}</div><div class=\"retailer\">{{retailer}}</div><div class=\"link\"><a href=\"{{link}}\">Click Here!</a></div></div>";
+					var injectResult = Mustache.to_html(resultTemplate, result);
+					$('#results').append(injectResult);
+					
+				
+				}
+}
+  }});}
